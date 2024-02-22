4d97aff98b40b7f6ea7c979b1706ea28e2618a4d
FevenBelay23 <fevenbelay942@gmail.com>
2024-02-23 00:25:33 +0300

    navbar inital

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const user = true;
+  return (
+    // <div>Navbar</div>
+    <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
+      <h1 className="text-lg md:text-xl font-extrabold">
+        <Link to="/">Blog Market</Link>
+      </h1>
+      <div className="flex justify-center items-center space-x-0">
+        <p>
+          <BsSearch />
+        </p>
+        <input
+          className="outline-none px-3"
+          placeholder="Search a post"
+          type="text"
+        />
+      </div>
+      <div className="flex items-center justify-center space-x-2 md:space-x-4">
+        {user ? (
+          <h3>
+            <Link to="/write">Write</Link>
+          </h3>
+        ) : (
+          <h3>
+            <Link to="/login"></Link>Login
+          </h3>
+        )}
+        {user ? (
+          <h3>Profile</h3>
+        ) : (
+          <h3>
+            <Link to="/register"></Link>Register
+          </h3>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
