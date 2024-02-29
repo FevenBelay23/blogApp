6a2bac2408ce7b49742780c22fcea9c71dac9812
FevenBelay23 <fevenbelay942@gmail.com>
2024-02-29 15:08:21 +0300

    database setup

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.post('/register', (req, res) => {
-    res.json('test ok1');
+    const{username, password} = req.body;
+    res.json({requestData:{username, password}});
 });
 
 app.listen(4000);
+// mongodb+srv://blog:7k2vL8mdnsRPan8a@cluster0.z7ykgom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// 7k2vL8mdnsRPan8a
